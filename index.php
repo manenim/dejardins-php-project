5a2bac65c0550482a6a96121b74658d66b1fcee9
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2022-10-26 23:48:16 +0100

    ready to deploy

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Log in to your account</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+ <section class="ctm-topbar">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">AA +</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Français</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Help</a>
+              </li>
+            </ul>
+          </div>
+        </nav> 
+      </div>
+    </div>
+  </div>
+ </section>
+  
+ <section class="ctm-logobar">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="ctm-logo">
+          <a href="#"><img src="images\mainlogo.png"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+ </section>
+
+ <section class="ctm-form-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="ctm-form">
+          <h2>Log in to your account</h2>
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="text" class="form-control">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Remember me</label> <span class="ctm-safe"><a href="#">(Is this safe?)</a></span>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+              <small id="emailHelp" class="form-text text-muted">Important: Passwords are case-sensitive.</small>
+              <a class="ctm-forgot" href="#">Forgot your password?</a>
+            </div>
+            <div class="ctm-button">
+              <button type="submit" class="btn btn-primary">Log in</button>
+            </div>
+          </form>
+        </div>
+
+        <div class="ctm-bottom">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="ctm-link">
+                <ul class="ctm-right-border">
+                  <li><a href="#">Register for AccèsD</a></li>
+                  <li><a href="#">Register for AccèsD  Affaires</a></li>
+                  <li><a href="#">Become a member</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="ctm-link">
+                <ul>
+                  <li><a href="#">Site security</a></li>
+                  <li><a href="#">Technical support</a></li>
+                  <li><a href="#">Report fraud</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="ctm-link">
+                <ul>
+                  <li><img src="images/lock.svg"><a href="#">Guaranteed 100% secure</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 ctm-image">
+        <!-- <div class="ctm-image">
+          <img src="images/img-auth-desj.jpg">
+        </div> -->
+      </div>
+
+    </div>
+  </div>
+ </section>
+
+
+ <footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="ctm-footer-links1">
+          <ul>
+            <li><a href="#">PERSONAL SERVICES</a></li>
+            <li><a href="#">BUSINESS SERVICES</a></li>
+            <li><a href="#">CO-OPME</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">MOBILE, GPS AND RSS</a></li>
+          </ul>
+        </div>
+        <hr>
+        <hr>
+        <div class="ctm-footer-links1 ctm-new-css">
+          <ul>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Terms of Use and legal notes</a></li>
+            <li><a href="#">Accessibility</a></li>
+            <li><a href="">Site map</a></li>
+          </ul>
+        </div>
+        <div class="ctm-copyright">
+          <p>© 1996-2022, Fédération des caisses Desjardins du Québec. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+ </footer>
+</body>
+
+   
